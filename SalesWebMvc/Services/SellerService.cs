3a438e92ac7cc749a72a59f4f2eb65609fb449f4
coelho1989 <coelho.jorge89@gmail.com>
2023-11-18 19:46:49 +0000

    Fix foreign key

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,5 +17,12 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller)
+        {
+            seller.Department = _context.Department.First();
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
