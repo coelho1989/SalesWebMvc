b3d78c59375c2e70a2d298c32ddec29ef3d7af86
coelho1989 <coelho.jorge89@gmail.com>
2023-11-18 17:29:25 +0000

    SellerService and service Find All

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
         builder => builder.MigrationsAssembly("SalesWebMvc")));
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
