22058044550ff5f537ddb9008081344fd8fe30dc
coelho1989 <coelho.jorge89@gmail.com>
2023-11-19 03:39:04 +0000

    Seller details

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Services
@@ -26,7 +27,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(s=>s.Id == id);
+            return _context.Seller.Include(s=>s.Department).FirstOrDefault(s=>s.Id == id);
         }
 
         public void Remove(int id)
